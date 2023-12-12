951f01c6a9f72aad1621d4d7baac887bff749bec
Gabriel <g.fioripas@gmail.com>
2023-12-11 21:43:10 -0300

    Agregando cambios, creando nuevos archivos y carpetas

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,47 @@
+from .models import PostCategory
+from .models import Post
+from .models import Category
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+admin.site.register(PostCategory)
+
+admin.site.register(Comment)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'color', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+    fieldsets = (
+        ('Category', {
+            'fields': ('name', 'description', 'color', 'slug')
+        }),
+    )
+
+
+class PostCategoryInLine(admin.TabularInline):
+    model = PostCategory
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content',
+                    'created_at', 'updated_at', 'status')
+    list_filter = ('status', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'created_at'
+    ordering = ('title',)
+    inlines = [PostCategoryInLine,]
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        ('Post', {
+            'fields': ('title', 'content', 'slug', 'author',
+                       'created_at', 'updated_at', 'status')
+        }),
+    )
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -14,30 +15,17 @@ class Category(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.TextField()
-    slug = models.SlugField(unique=True, blank=True)
     color = models.CharField(max_length=20, choices=COLOR_CHOICES)
-
-    def __str__(self):
-        return f'{self.name} ({self.description})'
+    slug = models.SlugField(max_length=120, unique=True, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        # Generar el slug automáticamente a partir del name
+        # Generar el slug automáticamente a partir del título
         if not self.slug:
             self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
-
-class User(models.Model):
-    # campos para users, usuario, email, bio, profile_picture, agregar otros de ser necesario.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=150)
-    email = models.EmailField()
-    bio = models.CharField(max_length=150)
-    profile_picture = models.ImageField(
-        upload_to='static/blog_personal/img/user', null=True, blank=True)
-
     def __str__(self):
-        return f'{self.user}  - {self.email}'
+        return f'{self.name} ({self.description})'
 
 
 class Post(models.Model):
@@ -58,7 +46,7 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='static/blog_personal/img/post', null=True, blank=True)
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True)
+        'accounts.UserModel', on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         # Generar el slug automáticamente a partir del título
@@ -76,3 +64,14 @@ class PostCategory(models.Model):
 
     def __str__(self):
         return self.post.title + " - " + self.category.name
+
+
+class Comment(models.Model):
+    author = models.ForeignKey('accounts.UserModel', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} - {self.post.title}"
